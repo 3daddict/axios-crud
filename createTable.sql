af7535ccfcad1e7f18a3c5f289cb00aed365f422
Michael Salvati <print@3daddict.com>
2019-05-09 11:39:45 -0700

    insert queries created and table syntax updated

--- createTable.sql
+++ createTable.sql
@@ -1,9 +1,10 @@
-CREATE TABLE users (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-phone INT(10),
-subscribe BOOLEAN,
-reg_date TIMESTAMP
-)
+CREATE TABLE `users` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL DEFAULT '',
+  `phone` varchar(30) NOT NULL,
+  `subscibe` boolean NOT NULL DEFAULT true,
+  `reg_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = latin1;
---
+++ insertUsers.sql
@@ -0,0 +1,17 @@
+INSERT INTO users (firstname, lastname, email, phone)
+VALUES ('Stephanie', 'Neff', 'StephanieSNeff@rhyta.com', '602-617-5897');
+
+INSERT INTO users (firstname, lastname, email, phone)
+VALUES ('Grace', 'Guinan', 'GraceRGuinan@jourrapide.com', '319-937-0636');
+
+INSERT INTO users (firstname, lastname, email, phone)
+VALUES ('John', 'Valentin', 'JohnMValentin@teleworm.us', '260-894-0527')
+
+INSERT INTO users (firstname, lastname, email, phone)
+VALUES ('Richard', 'Stack', 'RichardJStack@rhyta.com', '703-945-7263')
+
+INSERT INTO users (firstname, lastname, email, phone)
+VALUES ('Rena', 'Young', 'RenaMYoung@rhyta.com', '706-817-3584')
+
+INSERT INTO users (firstname, lastname, email, phone)
+VALUES ('Wayne', 'Hayman', 'WaynePHayman@armyspy.com', '925-244-4679')
